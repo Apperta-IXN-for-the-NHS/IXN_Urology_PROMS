799f21d249bd80a43b058aefb1586aee56c699a3
odean111 <oliver.dean92@gmail.com>
2020-03-11 16:29:17 +0000

    Info text for support

--- practice/src/pages/Support/Info.tsx
+++ practice/src/pages/Support/Info.tsx
@@ -1,8 +1,8 @@
 export let SupportInfo = [
-    {title: "Diet & Nutrition", description: "text goes here"},
-    {title: "Support Charities", description: "text goes here"},
-    {title: "Patient Communities", description: "text goes here"},
-    {title: "Counseilling", description: "text goes here"},
-    {title: "Exercise", description: "text goes here"},
+    {title: "Diet & Nutrition", description: "Differences in diet and lifestyle may account for the variability of prostate cancer rates in different countries. Good nutrition may help reduce the risk of developing prostate cancer, slow progression of the disease and prevent aggressive disease. The links below discuss a healthy diet for good prostate health and guidelines for a healthy diet while in treatment for prostate cancer. Diet and nutirtion should never be used as a replacement for treatment. If in doubt, discuss dietary changes with your doctor."},
+    {title: "Support Charities", description: "Charites such as Prostate Cancer UK are on the forefront of leading the fight against prostate cancer. They provide advice and support to those diagnosed with prostate cancer and their families, as well as directing and funding research into treatments and improved diagnostic tools."},
+    {title: "Patient Communities", description: "Meeting other people with similar experiences can be invaluable when you are affected by cancer. Self-help and support groups offer a chance for people to talk to others and come together with people who understand how they are feeling, share concerns and get advice from those who have gone through similar experiences."},
+    {title: "Counseilling", description: "The important thing to remember when you’re affected by prostate disease is that you’re not alone. There are lots of people out there who are on very similar journeys, and who have had the treatments and experiences you may be facing. Follow the links below to find one-to-one support resources."},
+    {title: "Exercise", description: "Exercise improves heart health and bone density, as well as decreases the risk of diabetes and obesity. In addition, exercise can decrease blood sugar levels, which lowers insulin levels and inflammation. That’s important because there appears to be an association between insulin levels, inflammation, and prostate cancer risk. The other big benefit of exercise is that it can reduce the side effects of common prostate cancer treatments."},
 ]
 
