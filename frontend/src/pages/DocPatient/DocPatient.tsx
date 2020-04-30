0629069b076f94d5f768e17de3710ac477398090
Omar-V2 <omardiab4@googlemail.com>
2020-04-30 03:46:31 +0100

    doc patient page UI redesign

--- frontend/src/pages/DocPatient/DocPatient.tsx
+++ frontend/src/pages/DocPatient/DocPatient.tsx
@@ -5,15 +5,21 @@ import {
   IonHeader,
   IonPage,
   IonToolbar,
+  IonItem,
+  IonLabel,
+  IonList,
   IonCard,
   IonCardHeader,
+  IonCardSubtitle,
   IonCardTitle,
   IonGrid,
   IonRow,
   IonCol,
   IonContent,
   IonIcon,
+  IonCardContent,
 } from "@ionic/react";
+import { mail, flask, barChart, reader } from "ionicons/icons";
 
 import "./DocPatient.css";
 import calImg from "../../assets/images/calendar.png";
@@ -26,30 +32,61 @@ import { peopleCircleOutline } from "ionicons/icons";
 interface QuadProps {
   title: string;
   image: any;
+  description: string;
   link: string;
   history: any;
 }
 
-const QuadCard: React.FC<QuadProps> = ({ title, image, history, link }) => {
+const QuadCard: React.FC<QuadProps> = ({
+  title,
+  image,
+  description,
+  history,
+  link,
+}) => {
   return (
-    <IonCol size="6">
-      <IonCard
-        className="doc"
+    <IonCard color="primary">
+      <IonItem
+        detail
+        lines="full"
+        button
         onClick={(e) => {
           e.preventDefault();
           history.push(link);
         }}
       >
-        <IonCardHeader>
-          <br />
-          <br />
-          <img src={image} alt={title} />
-          <IonCardTitle color="nhswhite" className="ion-text-center">
-            {<h4>{title}</h4>}
-          </IonCardTitle>
-        </IonCardHeader>
-      </IonCard>
-    </IonCol>
+        {/* <IonLabel className="ion-text-center">view</IonLabel> */}
+        <h2 className='ion-text-center'>{title}</h2>
+        <IonIcon slot="end" icon={image} color="white" />
+      </IonItem>
+      {/* <IonCardHeader>
+        <IonCardTitle>
+          {title}
+        </IonCardTitle>
+      </IonCardHeader> */}
+      <IonCardContent>
+        <p>{description}</p>
+      </IonCardContent>
+      {/* </IonItem> */}
+    </IonCard>
+    // <IonCol size="6">
+    //   <IonCard
+    //     className="doc"
+    //     onClick={(e) => {
+    //       e.preventDefault();
+    //       history.push(link);
+    //     }}
+    //   >
+    //     <IonCardHeader>
+    //       <br />
+    //       <br />
+    //       <img src={image} alt={title} />
+    //       <IonCardTitle color="nhswhite" className="ion-text-center">
+    //         {<h4>{title}</h4>}
+    //       </IonCardTitle>
+    //     </IonCardHeader>
+    //   </IonCard>
+    // </IonCol>
   );
 };
 
@@ -62,35 +99,37 @@ const DocPatient: React.FC<DocPatientProps> = ({ history }) => {
     [
       {
         title: "Letters",
-        image: letterImg,
+        image: mail,
         description:
           "Letters, documents and forms you have received regarding your treatment and diagnosis.",
         link: "/letters",
       },
       {
         title: "Questionnaires",
-        image: quesImg,
+        image: reader,
         description:
           "Important Questionnaires to help assist your medical treatment.",
         link: "/quest",
       },
-    ],
-    [
       {
         title: "My Medication",
-        image: sympImg,
+        image: flask,
         description:
           "Log information about your recent symptoms to give doctors more information about your wellbeing.",
         link: "/symptoms",
       },
       {
         title: "Results",
-        image: calImg,
-        description: "Input and organise all of your upcoming appointments",
+        image: barChart,
+        description:
+          "View and update results of medical tests you have undertaken ",
         link: "/results",
       },
     ],
   ];
+  const padLeft = {
+    paddingLeft: "20px",
+  };
   const [userInfo, setUserInfo] = useContext(UserContext as any);
   return (
     <IonPage>
@@ -98,9 +137,24 @@ const DocPatient: React.FC<DocPatientProps> = ({ history }) => {
         <IonToolbar></IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2 className="ion-padding-left">Welcome {userInfo.firstName}</h2>
-        {/* {console.log(userInfo)} */}
-        <IonGrid>
+        <h2 style={padLeft}>
+          Hello {userInfo.firstName}. <br /> How can we help?
+        </h2>
+        {cardContent.map((content, index) => (
+          <IonList>
+            {content.map((c, idx) => (
+              <QuadCard
+                key={idx}
+                title={c.title}
+                image={c.image}
+                description={c.description}
+                link={c.link}
+                history={history}
+              />
+            ))}
+          </IonList>
+        ))}
+        {/* <IonGrid>
           {cardContent.map((content, idx) => (
             <IonRow key={idx}>
               {content.map((c, idx) => (
@@ -114,7 +168,7 @@ const DocPatient: React.FC<DocPatientProps> = ({ history }) => {
               ))}
             </IonRow>
           ))}
-        </IonGrid>
+        </IonGrid> */}
       </IonContent>
     </IonPage>
   );
